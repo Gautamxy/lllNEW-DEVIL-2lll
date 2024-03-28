e14b75c2fc32fc271bcbe1bb864ff0ae9e71779f
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-03-28 07:20:06 +0600

    Update pinterest.js

--- modules/commands/pinterest.js
+++ modules/commands/pinterest.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "pic",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "AYAN",
+    credits: "AYAN CHOUDHURY",
     description: "Image search",
     commandCategory: "Search",
     usages: "[Text]",
@@ -13,10 +13,10 @@ module.exports.run = async function({ api, event, args }) {
     const fs = require("fs-extra");
     const request = require("request");
     const keySearch = args.join(" ");
-    if(keySearch.includes("-") == false) return api.sendMessage(' ◈ ━━━━━━ ⸙ ━━━━━━ ◈\n\n𝗬𝗼𝘂 𝗧𝘆𝗽𝗲𝘀 𝗪𝗿𝗼𝗻𝗴 𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗣𝗹𝗲𝗮𝘀𝗲 𝗨𝘀𝗲𝗱 𝗖𝗼𝗿𝗿𝗲𝗰𝘁 \n𝐄𝐗𝐀𝐌𝐏𝐋𝐄 :- ${global.config.PREFIX}Pinterest Bot - 10 \n\n◈ ━━━━━━ ⸙ ━━━━━━ ◈', event.threadID, event.messageID)
+    if(keySearch.includes("-") == false) return api.sendMessage('Please enter in the format, example: pic mia khalifa-10 (it depends on you how many images you want to appear in the result) credit by Shaon Ahmed', event.threadID, event.messageID)
     const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
-    const numberSearch = keySearch.split("-").pop() || 10
-    const res = await axios.get(`https://pagal.ch4nd.repl.co/pinterest?search=${encodeURIComponent(keySearchs)}`);
+    const numberSearch = keySearch.split("-").pop() || 6
+    const res = await axios.get(`https://shaon-all-api-07mo.onrender.com/pinterest?search=${encodeURIComponent(keySearchs)}`);
     const data = res.data.data;
     var num = 0;
     var imgData = [];
@@ -28,7 +28,7 @@ module.exports.run = async function({ api, event, args }) {
     }
     api.sendMessage({
         attachment: imgData,
-        body: '━━━━━━ ⸙ ━━━━━━\n' + numberSearch + ' 𝐒𝐄𝐀𝐑𝐂𝐇 𝐑𝐄𝐒𝐔𝐋𝐓𝐒 𝐅𝐎𝐑 𝐊𝐄𝐘𝐖𝐎𝐑𝐃𝐒 : '+ keySearchs + '\n━━━━━━ ⸙ ━━━━━━'
+        body: numberSearch + ' Searching 🔎 results for you. Your keyword: '+ keySearchs
     }, event.threadID, event.messageID)
     for (let ii = 1; ii < parseInt(numberSearch); ii++) {
         fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
