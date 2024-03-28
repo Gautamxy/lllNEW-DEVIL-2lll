a9a7d1cb7a7f6974c6ad329ad886d2deffcc05fb
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-03-28 23:10:43 +0600

    Update album.js

--- modules/commands/album.js
+++ modules/commands/album.js
@@ -1,95 +1,82 @@
+
 module.exports.config = {
-  name: "album",
+  name: "video2",
   version: "1.0.",
   hasPermssion: 0,
-  credits: "AYAN CHOWDHURY",//dont change credit😠
+  credits: "NAYAN/AYAN",//full credit NAYAN
   description: "rendom video for you",
   commandCategory: "rendom",
-  usages: "[AYAN ALBUM]",
+  usages: "[NAYAN ALBUM]",
   cooldowns: 0
-}; module.exports.run = async function({  event: e,
-  api: a,
-  args: n
+};
+
+
+module.exports.run = async function({
+	event: e,
+	api: a,
+	args: n
 }) {
-  if (!n[0]) return a.sendMessage("╭──────•♥︎𝘼𝙔𝘼𝙉 𝙖𝙡𝙗𝙪𝙢☺︎︎•──────╮\n\n🖤━━━━━━💟𝙰𝙻𝙱𝚄𝙼 𝚅𝙸𝙳𝙴𝙾💟━━━━━━🖤 \n!\n!➤1 𝙸𝚂𝙻𝙰𝙼 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤2 𝙰𝙽𝙸𝙼𝙴 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤3 𝚂𝙷𝙰𝙸𝚁𝙸 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤4 𝚂𝙷𝙾𝚁𝚃 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤5 𝚂𝙰𝙳𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤6 𝚂𝚃𝙰𝚃𝚄𝚂 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤7 𝙵𝙾𝙾𝚃𝙱𝙰𝙻𝙻 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤8 𝙵𝚄𝙽𝙽𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤9 𝙻𝙾𝚅𝙴 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤10 𝙲𝙿𝙻 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤11 𝙱𝙰𝙱𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤12 𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤13 𝙻𝙾𝙵𝙸 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤14 𝙷𝙰𝙿𝙿𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤15 𝙷𝚄𝙼𝙰𝙸𝚈𝚄𝙽 𝚂𝙸𝚁 𝚅𝙸𝙳𝙴𝙾◄┈╯\n🖤━━━━━━🫦𝙷𝙾𝚃 𝚅𝙸𝙳𝙴𝙾━━━━━━🖤\n!➤16 𝚂𝙴𝚇 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤17 𝙷𝙾𝚁𝙽𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤18 𝙸𝚃𝙴𝙼 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤19 𝙷𝙾𝚃 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤20 𝙲𝙰𝙿𝙲𝚄𝚃 𝚅𝙸𝙳𝙴𝙾◄┈╯\n━━━━━━━━━━━━━━━━━━━━━\n𝙾𝚆𝙽𝙴𝚁:♥︎𝘼𝙔𝘼𝙉 𝙖𝙡𝙗𝙪𝙢☺︎︎\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n𝙰 𝙿 𝙸//𝘼𝙔𝘼𝙉\n━━━━━━━━━━━━━━━━━━━━━\n╰──────•♥𝘼𝙔𝘼𝙉•──────╯\n\nTell me how many video numbers you want to see by replaying this message", e.threadID, ((a, n) => {
-    global.client.handleReply.push({
-      name: this.config.name,
-      messageID: n.messageID,
-      author: e.senderID,
+	if (!n[0]) return a.sendMessage("====「 𝐕𝐈𝐃𝐄𝐎 」====\n━━━━━━━━━━━━━\n𝟙. 𝐋𝐎𝐕𝐄 𝐕𝐈𝐃𝐄𝐎 💞 \n𝟚. 𝐂𝐎𝐔𝐏𝐋𝐄 𝐕𝐈𝐃𝐄𝐎 💕\n𝟛. 𝐒𝐇𝐎𝐑𝐓 𝐕𝐈𝐃𝐄𝐎 📽\n𝟜. 𝐒𝐀𝐃 𝐕da𝐄𝐎 😔\n𝟝. 𝐒𝐓𝐀𝐓𝐔𝐒 𝐕𝐈𝐃𝐄𝐎 📝\n𝟞. 𝐒𝐇𝐀𝐈𝐑𝐈\n𝟟. 𝐁𝐀𝐁𝐘 𝐕𝐈𝐃𝐄𝐎 😻\n𝟠. 𝐀𝐍𝐈𝐌𝐄 𝐕𝐈𝐃𝐄𝐎 \n𝟡. 𝐇𝐔𝐌𝐀𝐈𝐘𝐔𝐍 𝐅𝐎𝐑𝐈𝐃 𝐒𝐈𝐑 ❄\n𝟙𝟘. 𝐈𝐒𝐋𝐀𝐌𝐈𝐊 𝐕𝐈𝐃𝐄𝐎 🤲\n\n===「 𝟏𝟖+ 𝐕𝐈𝐃𝐄𝐎 」===\n━━━━━━━━━━━━━\n𝟙𝟙. 𝐇𝐎𝐑𝐍𝐘 𝐕𝐈𝐃𝐄𝐎 🥵\n𝟙𝟚. 𝐇𝐎𝐓 🔞\n𝟙𝟛. 𝐈𝐓𝐄𝐌\n\nTell me how many video numbers you want to see by replaying this message", e.threadID, ((a, n) => {
+		global.client.handleReply.push({
+			name: this.config.name,
+			messageID: n.messageID,
+			author: e.senderID,
       type: "create"
-    })
-  }), e.messageID)
-  }, module.exports.handleReply = async ({
-  api: e,
-  event: a,
-  client: n,
-  handleReply: t,
-  Currencies: s,
-  Users: i,
-  Threads: o
-  }) => {
-  var { p, h } = linkanh();
+		})
+	}), e.messageID)
+}, module.exports.handleReply = async ({
+	api: e,
+	event: a,
+	client: n,
+	handleReply: t,
+	Currencies: s,
+	Users: i,
+	Threads: o
+}) => {
+	var { p, h } = linkanh();
+  const request = require("request");
+	if ("create" === t.type) {
+		const n = (await p.get(h)).data.data;
+    const cap = (await p.get(h)).data.nayan;
+    const cn = (await p.get(h)).data.count;
+		let t = (await p.get(n, {
+			responseType: "stream"
+		})).data;
+		return e.sendMessage({
+			body: `${cap}` + `\n\n¤《𝐓𝐎𝐓𝐀𝐋 𝐕𝐈𝐃𝐄𝐎: ${cn}》¤`,
+			attachment: t
+		}, a.threadID, a.messageID)
+	}
 
-  if ("create" === t.type) {
-    const n = (await p.get(h)).data.data;
-    const shaon = (await p.get(h)).data.shaon;
-    const ls = (await p.get(h)).data.count;
-    let t = (await p.get(n, {
-      responseType: "stream"
-    })).data;
-    return e.sendMessage({
-      body: `🟡${shaon}\nVideo Number:${ls}\nAPI ♥︎𝘼𝙔𝘼𝙉 𝙖𝙡𝙗𝙪𝙢☺︎︎`,
-      attachment: t
-    }, a.threadID, a.messageID)
-  }
-
-  function linkanh() {
+    function linkanh() {
         const p = require("axios");
+        const n = "http://nl2-3.deploy.sbs:2011";
         if ("1" == a.body)
-            var h = "https://noobs-apihouse.onrender.com/dipto/album/islam";
+            var h = `${n}/video/love`;
         else if ("2" == a.body)
-         var   h = "https://noobs-apihouse.onrender.com/dipto/album/anime";
+         var   h = `${n}/video/cpl`;
         else if ("3" == a.body)
-         var   h = "https://noobs-apihouse.onrender.com/dipto/album/shairi";
+         var   h = `${n}/video/shortvideo`;
         else if ("4" == a.body)
-          var  h = "https://noobs-apihouse.onrender.com/dipto/album/short";
+          var  h = `${n}/video/sadvideo`;
         else if ("5" == a.body)
-          var  h = "https://noobs-apihouse.onrender.com/dipto/album/sad";
+          var  h = `${n}/video/status`;
         else if ("6" == a.body)
-          var  h = "https://noobs-apihouse.onrender.com/dipto/album/status";
+          var  h = `${n}/video/shairi`;
         else if ("7" == a.body)
-          var  h = "https://noobs-apihouse.onrender.com/dipto/album/football";
+          var  h = `${n}/video/baby`;
         else if ("8" == a.body)
-          var  h = "https://noobs-apihouse.onrender.com/dipto/album/funny";
+          var  h = `${n}/video/anime`;
         else if ("9" == a.body)
-         var   h = "https://noobs-apihouse.onrender.com/dipto/album/love";
+         var   h = `${n}/video/humaiyun`;
         else if ("10" == a.body)
-         var  h = "https://noobs-apihouse.onrender.com/dipto/album/cpl";
-          else if ("11" == a.body)
-           var  h = "https://noobs-apihouse.onrender.com/dipto/album/baby";
-          else if ("12" == a.body)
-          var  h = "https://noobs-apihouse.onrender.com/dipto/album/kosto";
-         else if ("13" == a.body)
-         var  h = "https://noobs-apihouse.onrender.com/dipto/album/lofi";
-         else if ("14" == a.body)
-         var  h = "https://noobs-apihouse.onrender.com/dipto/album/happy";
-         else if ("15" == a.body)
-         var  h = "https://noobs-apihouse.onrender.com/dipto/album/humaiyun";
-           else if ("16" == a.body)
-         var  h =
-"https://noobs-apihouse.onrender.com/dipto/album/sex";
-    else if ("17" == a.body)
-         var  h =
-"https://noobs-apihouse.onrender.com/dipto/album/horny";
-    else if ("18" == a.body)
-         var  h =
-"https://noobs-apihouse.onrender.com/dipto/album/item";
-    else if ("19" == a.body)
-         var  h =
-"https://noobs-apihouse.onrender.com/dipto/album/hot";
-    else if ("20" == a.body)
-         var  h =
-"https://noobs-apihouse.onrender.com/dipto/album/capcut";
+         var  h = `${n}/video/islam`;
+        else if ("11" == a.body)
+         var  h = `${n}/video/horny`;
+        else if ("12" == a.body)
+          var  h = `${n}/video/hot`;
+        else if ("13" == a.body)
+          var  h = `${n}/video/item`;
         return { p, h };
     }
 };
