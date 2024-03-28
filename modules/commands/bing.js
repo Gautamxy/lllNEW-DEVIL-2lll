9f0cc0a7028bbcce3b5494c955722a8e053cb10f
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-03-28 16:01:46 +0600

    Create bing.js

---
+++ modules/commands/bing.js
@@ -0,0 +1,63 @@
+ module.exports.config = {
+    name: "bing",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "NAYAN/AYAN",//full credit NAYAN//edit AYAN
+    description: "Apply code from buildtooldev and pastebin",
+    commandCategory: "auto",
+    usages: "bing text to reply image",
+    cooldowns: 0,
+    dependencies: {
+        "pastebin-api": "",
+        "cheerio": "",
+        "request": ""
+    }
+};
+
+   languages: {
+   "vi": {},
+       "en": {
+           "missing": 'use : /bing cat'
+       }
+   },
+
+start: async function({ nayan, events, args, lang}) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const request = require("request");
+    const prompt = args.join(" ");
+    const key = this.config.credits;
+    const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
+  const n = apis.data.api2
+    if(!prompt) return nayan.reply(lang('missing'), events.threadID, events.messageID)
+
+  const rndm = ['cookie'] // input your cookie hare
+
+  var cookie = rndm[Math.floor(Math.random() * rndm.length)];
+
+
+    const res = await axios.get(`${n}/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+
+  
+  console.log(res.data)
+    const data = res.data.result;
+  const numberSearch = data.length
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
+    }
+
+    
+    nayan.reply({
+        attachment: imgData,
+        body: "🔍Bing Search Result🔍\n\n📝Prompt: " + prompt + "\n\n#️⃣Number of Images: " + numberSearch
+    }, events.threadID, events.messageID)
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+    }
+}
+   }
