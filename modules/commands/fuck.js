76b87e408516108ef9f1d928312b6d1b4329d6ae
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-03 21:25:05 +0600

    Update fuck.js

--- modules/commands/fuck.js
+++ modules/commands/fuck.js
@@ -1,8 +1,8 @@
 module.exports.config = {
     name: "fuck",
-    version: "7.3.1",
-    hasPermssion: 0,
-    credits: "AYAN",
+    version: "3.1.1",
+    hasPermssion: 2,
+    credits: "Ayan",
     description: "Get fuck",
     commandCategory: "nsfw",
     usages: "[@mention]",
@@ -20,9 +20,9 @@ module.exports.onLoad = async() => {
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'fuckv2.png');
+    const path = resolve(__dirname, 'cache/canvas', 'fuckv3.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/Ee93Xjb.jpeg", path);
+    if (!existsSync(path)) await downloadFile("https://i.ibb.co/TW9Kbwr/images-2022-08-14-T183542-356.jpg", path);
 }
 
 async function makeImage({ one, two }) {
@@ -32,7 +32,7 @@ async function makeImage({ one, two }) {
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let batgiam_img = await jimp.read(__root + "/fuckv2.png");
+    let batgiam_img = await jimp.read(__root + "/fuckv3.png");
     let pathImg = __root + `/batman${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(180, 180), 190, 200).composite(circleTwo.resize(180, 180), 390, 200);
+    batgiam_img.composite(circleOne.resize(100, 100), 20, 300).composite(circleTwo.resize(150, 150), 100, 20);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
