ac7acd8787ab0cd4c5b596e7e2920d1ff210c0c1
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-03 19:30:48 +0600

    Update autodl.js

--- modules/commands/autodl.js
+++ modules/commands/autodl.js
@@ -1,65 +1,41 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-const tinyurl = require('tinyurl');
-
 module.exports.config = {
   name: "autodl",
   version: "1.0.",
   hasPermssion: 0,
-  credits: "AYAN",
-  description: "Fb Vid Downloader",
-  commandCategory: "all download",
-  usages: "fb video url",
-   usePrefix: true,
-  cooldowns: 2
+  credits: "AYAN CHOWDHURY",//dont change credit😠
+  description: "All  Video Downloader",
+  commandCategory: "Downloader",
+  usages: "[video link]",
+  cooldowns: 0
 };
 
 module.exports.handleEvent = async function ({ api, event, client, __GLOBAL }) {
-let ayan = event.body ? event.body : '';
+  const axios = require('axios');
+const fs = require('fs-extra');
+let shaon = event.body ? event.body : '';
   try {
-if (ayan.startsWith('https://vt.tiktok.com') ||
-ayan.startsWith("https://vm.tiktok.com") ||
-ayan.startsWith('https://www.facebook.com') || 
-ayan.startsWith('https://fb.watch')||
-ayan.startsWith('https://www.instagram.com/')|| ayan.startsWith('https://youtu.be/') ||
-ayan.startsWith('https://www.instagram.com/p/') || ayan.startsWith('https://pin.it/') || ayan.startsWith('https://youtube.com/') || ayan.startsWith('https://www.capcut.com/') || ayan.startsWith('https://www.threads.net/') || ayan.startsWith('https://twitter.com/') || ayan.startsWith('https://x.com/') || ayan.startsWith('https://l.likee.video/')){
-  api.sendMessage("wait baby", event.threadID, event.messageID);
-  if (!ayan) {
-    api.sendMessage("please put a valid fb video link", event.threadID, event.messageID);
+if (shaon.startsWith('https://vt.tiktok.com') ||
+    shaon.startsWith("https://vm.tiktok.com") ||
+    shaon.startsWith('https://www.facebook.com') || 
+    shaon.startsWith('https://fb.watch')||
+    shaon.startsWith('https://www.instagram.com/')|| shaon.startsWith('https://youtu.be/') || shaon.startsWith('https://youtube.com/')){
+  api.sendMessage("downloading video, please wait...", event.threadID, event.messageID);
+  if (!shaon) {
+    api.sendMessage("please put a valid video link", event.threadID, event.messageID);
     return;
     }
-const aa = await axios.get(`${global.config.API}/ayan/alldl?url=${encodeURIComponent(ayan)}`);
+    const path = __dirname + `/cache/shaonn.mp4`;
+    const aa = await axios.get(`https://noobs-api.onrender.com/api/download/fb2?url=(url)&apikey=SlI7c9RAS5`);
    const bb = aa.data;
-   const shortUrl = await tinyurl.shorten(bb.result);
-   const MSG = `✅ 🔗 Download Url: ${shortUrl}`;
-   let ex;
-   let cp;
-        if (bb.result.includes('.jpg')){
-             ex = ".jpg";
-             cp = "Here's your Photo <😘";
-        }
-        else if (bb.result.includes('.png')){
-             ex = ".png";
-             cp = "Here's your Photo <😘";
-        }
-        else if (bb.result.includes('.jpeg')){
-             ex = ".jpeg";
-             cp = "Here's your Photo <😘";
-        }
-        else { 
-          ex = ".mp4";
-          cp = bb.cp;
-        }
-const path = __dirname + `/cache/video${ex}`;
     const vid = (await axios.get(bb.result, { responseType: "arraybuffer", })).data;
     fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
     api.sendMessage({
-      body: `${cp}\n${MSG}\n 👑👑`,
+      body: `${bb.cp}`,
       attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
-if (ayan.startsWith('https://i.imgur.com')){
-  const ayan3= dipto.substring(ayan.lastIndexOf('.'));
-  const response = await axios.get(ayan, { responseType: 'arraybuffer' });
-const filename = __dirname + `/cache/ayan${ayan3}`;
+if (shaon.startsWith('https://i.imgur.com')){
+  const shaon3 = shaon.substring(shaon.lastIndexOf('.'));
+  const response = await axios.get(shaon, { responseType: 'arraybuffer' });
+const filename = __dirname + `/cache/shaon${shaon3}`;
     fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
     api.sendMessage({body: `Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
   () => fs.unlinkSync(filename),event.messageID)
@@ -70,4 +46,4 @@ api.sendMessage(`${e}`, event.threadID, event.messageID);
 };
 module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-}
+      }
