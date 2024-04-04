d7b1237c6b7bb9c2581f8f7cd11c0759ab8c50ce
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-04 14:40:29 +0600

    Update and rename hug3.js to kiss2.js

--- modules/commands/kiss2.js
+++ modules/commands/kiss2.js
@@ -1,9 +1,9 @@
 module.exports.config = {
-    name: "hug3",
+    name: "kiss2",
     version: "7.3.1",
     hasPermssion: 0,
     credits: "AYAN",
-    description: "Hug 🥰",
+    description: "kiss",
     commandCategory: "img",
     usages: "[@mention]",
     cooldowns: 5,
@@ -18,11 +18,11 @@ module.exports.config = {
 module.exports.onLoad = async() => {
     const { resolve } = global.nodemodule["path"];
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { downloadFiদেওle } = global.utils;
+    const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'hugv3.png');
+    const path = resolve(__dirname, 'cache/canvas', 'kissv3.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/7lPqHjw.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/3laJwc1.jpg", path);
 }
 
 async function makeImage({ one, two }) {
@@ -32,7 +32,7 @@ async function makeImage({ one, two }) {
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let batgiam_img = await jimp.read(__root + "/hugv3.png");
+    let batgiam_img = await jimp.read(__root + "/kissv3.png");
     let pathImg = __root + `/batman${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(220, 220), 200, 50).composite(circleTwo.resize(220, 220), 490, 200);
+    batgiam_img.composite(circleOne.resize(350, 350), 200, 300).composite(circleTwo.resize(350, 350), 600, 80);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
