f90db8b421f4e8070f7531793747b3b927b4a263
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-04 17:09:56 +0600

    Update antiout.js

--- modules/events/antiout.js
+++ modules/events/antiout.js
@@ -1,22 +1,22 @@
 module.exports.config = {
-    name: "antiout",
-    eventType: ["log:unsubscribe"],
-    version: "0.0.1",
-    credits: "DungUwU",
-    description: "Listen events"
+ name: "antiout",
+ eventType: ["log:unsubscribe"],
+ version: "0.0.1",
+ credits: " AYAN CHOWDHURY",
+ description: "Listen events"
 };
 
 module.exports.run = async({ event, api, Threads, Users }) => {
-    let data = (await Threads.getData(event.threadID)).data || {};
-    if (!data.antiout) return;
-    if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-    const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-    const type = (event.author == event.logMessageData.leftParticipantFbId) ? "tự rời" : "bị quản trị viên đá";
-    if (type == "tự rời") {
-        api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
-            if (error) {
-                api.sendMessage(`𝐒𝐨𝐑𝐫𝐲 ${name} 𝐁𝐚𝐛𝐲 𝐓𝐮𝐦 𝐊𝐨 𝐀𝐝𝐝 𝐧𝐢 𝐤𝐑 𝐩𝐚𝐘𝐚😢`, event.threadID)
-            } else api.sendMessage(` 𝐌𝐞𝐑𝐲 𝐇𝐨𝐭𝐘 𝐡𝐮𝐘 ${name} 𝐁𝐚𝐛𝐘 𝐓𝐮𝐦 𝐋𝐞𝐟𝐭 𝐧𝐢 𝐤𝐫 𝐬𝐊𝐭𝐲😎🌿`, event.threadID);
-        })
-    }
-}
+ let data = (await Threads.getData(event.threadID)).data || {};
+ if (data.antiout == false) return;
+ if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+ const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
+ if (type == "self-separation") {
+  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
+   if (error) {
+    api.sendMessage(`ওই ${name} লিভ নিয় না 🥺..আমাকে ছেড়ে কোথায় যাও🥺🥺add দিমু আবার☺️🥀 লিভ নিয় না পিলিজ আমার সাথে কথা বলো..!!😘🍼`, event.threadID)
+   } else api.sendMessage(`ওই ${name} লিভ নিয় না 🥺\n\nআমাকে ছেড়ে কোথায় যাও🥺🥺\n\nadd দিমু আবার☺️🥀 লিভ নিয় না পিলিজ\n\nআমার সাথে কথা বলো..!!😘🍼`, event.threadID);
+  })
+ }
+       }
