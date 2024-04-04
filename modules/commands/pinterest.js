79dfbad486673214e58aa60b01051bad90b9cd60
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-04 06:57:44 +0600

    Update pinterest.js

--- modules/commands/pinterest.js
+++ modules/commands/pinterest.js
@@ -2,10 +2,10 @@ module.exports.config = {
     name: "pic",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "AYAN CHOUDHURY",
+    credits: "AYAN CHOWDHURY",
     description: "Image search",
     commandCategory: "Search",
-    usages: "[Text]",
+    usages: "[Text] - [number]",
     cooldowns: 0,
 };
 module.exports.run = async function({ api, event, args }) {
@@ -13,24 +13,24 @@ module.exports.run = async function({ api, event, args }) {
     const fs = require("fs-extra");
     const request = require("request");
     const keySearch = args.join(" ");
-    if(keySearch.includes("-") == false) return api.sendMessage('Please enter in the format, example: pic mia khalifa-10 (it depends on you how many images you want to appear in the result) credit by Shaon Ahmed', event.threadID, event.messageID)
+    if(keySearch.includes("-") == false) return api.sendMessage(`Look my type:\n \n➤ ${global.config.PREFIX}nobita pic - 10\n \nTry it)`, event.threadID, event.messageID)
     const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
     const numberSearch = keySearch.split("-").pop() || 6
-    const res = await axios.get(`https://shaon-all-api-07mo.onrender.com/pinterest?search=${encodeURIComponent(keySearchs)}`);
+    const res = await axios.get(`https://bot.api-johnlester.repl.co/pinterest?search=${encodeURIComponent(keySearchs)}`);
     const data = res.data.data;
     var num = 0;
     var imgData = [];
     for (var i = 0; i < parseInt(numberSearch); i++) {
-      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let path = __dirname + `/data/${num+=1}.jpg`;
       let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
       fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
-      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
+      imgData.push(fs.createReadStream(__dirname + `/data/${num}.jpg`));
     }
     api.sendMessage({
         attachment: imgData,
-        body: numberSearch + ' Searching 🔎 results for you. Your keyword: '+ keySearchs
+        body: numberSearch + ' Search results for photo: '+ keySearchs
     }, event.threadID, event.messageID)
     for (let ii = 1; ii < parseInt(numberSearch); ii++) {
-        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+        fs.unlinkSync(__dirname + `/data/${ii}.jpg`)
     }
 };
