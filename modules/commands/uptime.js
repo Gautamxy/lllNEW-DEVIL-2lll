bc0ab93c3e808cf2925b661f4efad5cdf439ec56
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-05 09:36:15 +0600

    Update uptime.js

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -1,23 +1,42 @@
 module.exports.config = {
-  name: "upt",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "AYAN",
-  hide: true,
-  description: "Random images by api - upt",
-  commandCategory: "administrators",
-  cooldowns: 2,
-  dependencies: {
-    "pidusage": ""
-  }
+	name: "uptime",
+	version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Mirai - JRT",
+	description: "Check the time the bot was online",
+	commandCategory: "system",
+	cooldowns: 5,
+	dependencies: {
+		"pidusage": ""
+	}
 };
-  onStart: async function ({ api, event, args }) {
-    const uptime = process.uptime();
-    const seconds = Math.floor(uptime % 60);
-    const minutes = Math.floor((uptime / 60) % 60);
-    const hours = Math.floor((uptime / (60 * 60)) % 24);
-    const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
-    api.sendMessage(`Hello Master, the bot has been running for ${uptimeString}.`, event.threadID);
-  }
-};
+
+function byte2mb(bytes) {
+	const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	let l = 0, n = parseInt(bytes, 10) || 0;
+	while (n >= 1024 && ++l) n = n / 1024;
+	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
+}
+
+module.exports.run = async ({ api, event }) => {
+	const axios = require('axios');
+  const request = require('request');	
+	const fs = require("fs");
+  const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+  const pidusage = await global.nodemodule["pidusage"](process.pid);
+	const moment = require("moment-timezone");
+    var gio = moment.tz("Asia/Manila").format("D/MM/YYYY || HH:mm:ss");
+	const timeStart = Date.now();
+	let today = new Date();
+  axios.get('https://naughty.ocvat2810.repl.co').then(res => {
+	let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
+	let callback = function () {
+					api.sendMessage({body: `📅Today is: ${gio}\n🤖Bot is up and running ${hours} hour ${minutes} minute ${seconds} second ❤️.\n⚔Prefix: ${global.config.PREFIX}\n🔥Version: 2.0.0\n✅Total users: ${global.data.allUserID.length}\n🦖Total Group: ${global.data.allThreadID.length}\n⚡CPU in use: ${pidusage.cpu.toFixed(1)}\n⚠️Ram in use: ${byte2mb(pidusage.memory)}\n🔰Ping: ${Date.now() - timeStart}ms`, attachment: fs.createReadStream(__dirname + `/cache/anh.${ext}`)
+					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/anh.${ext}`), event.messageID);
+				};
+				request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/anh.${ext}`)).on("close", callback);
+			})
+                                  }
