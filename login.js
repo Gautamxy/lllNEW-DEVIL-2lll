efc764ccb05bf31eba94616983214607d4d086f0
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-06 16:33:25 +0600

    Update login.js

--- login.js
+++ login.js
@@ -6,5 +6,5 @@ var credentials = {email: "FB_EMAIL", password: "FB_PASSWORD"}; // credential in
 login(credentials, (err, api) => {
     if(err) return console.error(err);
     // login
-    fs.writeFileSync('Ayanstate.json', JSON.stringify(api.getAyanState())); //create Ayanstate
+    fs.writeFileSync('ayanstate.json', JSON.stringify(api.getappState())); //create ayanstate
 });
