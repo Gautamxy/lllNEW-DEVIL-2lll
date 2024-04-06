a2b97944012046b83768bdac550d50b6d8523b3b
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-06 16:08:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ logger("Opened server site...", "[ Starting ]");
 function startBot(message) {
     (message) ? logger(message, "[ Starting ]") : "";
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "CHAND.js"], {
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "AYAN.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
