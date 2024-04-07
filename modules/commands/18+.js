ae009304738c6cd35930d91d84be6d2cba55161f
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-08 02:14:19 +0600

    Update 18+.js

--- modules/commands/18+.js
+++ modules/commands/18+.js
@@ -240,6 +240,6 @@ const fs = global.nodemodule["fs-extra"];
 
 "https://i.postimg.cc/fR6KgQHC/big-boobs-of-sexy-Pakistani-girl-exposed.jpg",
     ];
-   var callback = () => api.sendMessage({body:` পতিতার ভারা ঘন্টায় ৫০০😋+\nSố ảnh: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+   var callback = () => api.sendMessage({body:`⚠️ | 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥 𝐭𝐡𝐢𝐬 18+ 𝐏𝐡𝐨𝐭𝐨..🥀+\nAYAN: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
