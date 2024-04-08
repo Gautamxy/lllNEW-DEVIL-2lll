b1cd23be29a77996eedc50960ee7d3b961a6394a
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-08 14:45:59 +0600

    Update restart.js

--- modules/commands/restart.js
+++ modules/commands/restart.js
@@ -1,35 +1,23 @@
 module.exports.config = {
-    name: "restart",
-    version: "2.0.2",
-    hasPermssion: 2,
-    credits: "MR CHAND",
-    description: "restart bot",
-    commandCategory: "admin-bot",
-    usages: "restart",
-    cooldowns: 5,
-    dependencies: { }
-}
- 
-module.exports.run = async function({ api, args, Users, event}) {
-const { threadID, messageID } = event;
-const axios = global.nodemodule["axios"];
+  name: "restart",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "mirai",
+  description: "Restart the Bot",
+  commandCategory: "system",
+  usages: "",
+  cooldowns: 5
+};
 
-const moment = require("moment-timezone");
-    var gio = moment.tz("Asia/Karachi").format("HH");
-    var phut = moment.tz("Asia/Karachi").format("mm");
-    var giay = moment.tz("Asia/Karachi").format("ss");
-const fs = require("fs");
-    let name = await Users.getNameUser(event.senderID)
-  if (event.senderID != 100072727941471) return api.sendMessage(`[❗] Restart started `, event.threadID, event.messageID)
-if(args.length == 0) api.sendMessage(`=== 『 𝗔𝗨𝗧𝗢 𝗥𝗘𝗦𝗧𝗔𝗥𝗧  』 ====\n━━━━━━━━━━━━━━━━━━\
-
-➝『📢』𝐍𝐀𝐌𝐄 𝐑𝐄𝐒𝐓𝐀𝐑𝐓 : ${name} \n
-➝『😍』𝐖𝐀𝐈𝐓 30 𝐒𝐄𝐂𝐎𝐍𝐃𝐒 𝐀𝐋𝐋 𝐁𝐎𝐓 𝐀𝐑𝐄 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐈𝐍𝐆 ..... `,event.threadID, () =>process.exit(1))
-else{    
-let time = args.join(" ");
-setTimeout(() =>
-api.sendMessage(`➝『📢』𝗕𝗼𝘁 𝘀𝗲̃ 𝗸𝗵𝗼̛̉𝗶 đ𝗼̣̂𝗻𝗴 𝗹𝗮̣𝗶 𝘀𝗮𝘂 : ${gio}s\n➝『⏰』𝗕𝗮̂𝘆 𝗴𝗶𝗼̛̀ 𝗹𝗮̀ : ${gio}:${phut}:${giay} `, threadID), 0)
-setTimeout(() =>
-api.sendMessage("➝『⏳』Đ𝗮𝗻𝗴 𝗯𝗮̆́𝘁 đ𝗮̂̀𝘂 𝗾𝘂𝗮́ 𝘁𝗿𝗶̀𝗻𝗵 𝗸𝗵𝗼̛̉𝗶 đ𝗼̣̂𝗻𝗴 𝗹𝗮̣𝗶",event.threadID, () =>process.exit(1)), 1000*`${time}`);
-}
-}
+module.exports.run = async function ({ api, args, Users, event }) {
+  var mention = Object.keys(event.mentions)[0];
+  let name = event.mentions[mention];
+  var arraytag = [];
+  arraytag.push({ id: mention });
+  var a = function (a) { api.sendMessage(a, event.threadID); }
+  a("✅𝙔𝙤𝙪𝙧 𝙬𝙞𝙨𝙝 𝙞𝙨 𝙢𝙮 𝙘𝙤𝙢𝙢𝙖𝙣𝙙, 𝙍𝙚𝙨𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙞𝙣..");
+  setTimeout(() => { a({ body: "3.." }) }, 5000);
+  setTimeout(() => { a({ body: "2.." }) }, 10000);
+  setTimeout(() => { a({ body: "1.." }) }, 15000);
+  setTimeout(() => { api.sendMessage("⏳𝙋𝙡𝙚𝙖𝙨𝙚 𝙬𝙖𝙞𝙩 𝙛𝙤𝙧 𝙖 𝙢𝙞𝙣𝙪𝙩𝙚, 𝙍𝙚𝙗𝙤𝙤𝙩𝙞𝙣𝙜 𝙨𝙮𝙨𝙩𝙚𝙢..", event.threadID, () => process.exit(1)) }, 20000);
+};
