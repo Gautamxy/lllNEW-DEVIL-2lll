9a0577ecb41b52ce2db91752d072221c11be9c93
AYAN CHOWDHURY <163991568+Ariyan-MA@users.noreply.github.com>
2024-04-21 11:25:14 +0600

    Update imgur.js

--- modules/commands/imgur.js
+++ modules/commands/imgur.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "imgur",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "AYAN CHOUDHURY",
+  credits: "NAYAN",// cmd convert to MR.AYAN **//
   description: "Create Image&video link",
   commandCategory: "Image",
   cooldowns: 1,
@@ -13,17 +13,20 @@ module.exports.config = {
   }
 };
 
+
 module.exports.run = async ({ api, event, args }) => {
     const axios = global.nodemodule['axios'];
+  const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
+  const n = apis.data.api
     const linkanh = event.messageReply.attachments[0].url || args.join(" ");
     if (!linkanh)
-        return api.sendMessage('Please give reply to a video/image or enter the image/video link', event.threadID, event.messageID);
+        return api.sendMessage('[âšœï¸]âžœ Please give feedback or enter the image or vide link', event.threadID, event.messageID);
     try {
-
-        const allPromise = (await Promise.all(event.messageReply.attachments.map(item => axios.get(`http://nl2-3.deploy.sbs:2011/imgurv2?link=${encodeURIComponent(item.url)}`)))).map(item => item.data.uploaded.image);
-        return api.sendMessage(`𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐂𝐫𝐞𝐚𝐭𝐞d 𝐘𝐨𝐮𝐫 𝐈𝐦𝐠𝐮𝐫 𝐏𝐨𝐫𝐭 𝐋𝐢𝐧𝐤✨🥀\n\n` + allPromise.join('"\n"') , event.threadID, event.messageID);
+      var tpk = `",`;
+        const allPromise = (await Promise.all(event.messageReply.attachments.map(item => axios.get(`${n}/imgurv2?link=${encodeURIComponent(item.url)}`)))).map(item => item.data.uploaded.image);
+        return api.sendMessage(`𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 𝖢𝗋𝖾𝖺𝗍𝖾𝖽 𝖸𝗈𝗎𝗋 𝖨𝗆𝗀𝗎𝗋 𝖴𝗋𝗅 𝖫𝗂𝗇𝗄✨🥀\n\n"` + allPromise.join('"\n"') + tpk, event.threadID, event.messageID);
     }
     catch (e) {
-        return api.sendMessage(' An error occurred while executing the command', event.threadID, event.messageID);
+        return api.sendMessage('[âšœï¸]âžœ An error occurred while executing the command', event.threadID, event.messageID);
     }
-};
+}; 
