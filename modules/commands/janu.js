c8bf67c6c6d64ddf592b53e2da85ac278add33b7
raihanbot2 <azizulislam20022r@gmail.com>
2024-07-04 02:45:17 +0600

    Update janu.js

--- modules/commands/janu.js
+++ modules/commands/janu.js
@@ -15,10 +15,10 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("raihan")==0 || event.body.indexOf("Rayhan")==0 || event.body.indexOf("রায়হান")==0 || event.body.indexOf("RAIHAN")==0) {
 		var msg = {
 				body: "─༅༎•রায়হান জনু তাহ এখন বিজি আছে࿐❥\n\n(༎ຶٹ༎ຶ) 𝗫𝗔𝗜𝗞𝗢 𝗥𝗔𝗜𝗛𝗔𝗡 𝗫.𝗫.𝗫 (༎ຶٹ༎ຶ)",
-				attachment: fs.createReadStream(__dirname + `/janu.mp3`)
+				attachment: fs.createReadStream(__dirname + `janu.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😊", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
