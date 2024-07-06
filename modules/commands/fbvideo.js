d11bf1204fc3b3e2801aff22d7e0f3bd317c5048
raihanbot2 <azizulislam20022r@gmail.com>
2024-07-06 17:38:45 +0600

    Update fbvideo.js

--- modules/commands/fbvideo.js
+++ modules/commands/fbvideo.js
@@ -1,62 +1,27 @@
-module.exports = {
-  config:{
-    name: "fbvideo",
-    version: "0.0.2",
-    permission: 0,
-    prefix: true,
-    credits: "Nayan",
-    description: "fb video",
-    category: "user",
-    usages: "",
-    cooldowns: 5,
-},
-
-  languages: {
-    "vi": {},
-        "en": {
-            "missing": '[ ! ] Input link.',
-            "wait": '𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝐕𝐈𝐃𝐄𝐎 𝐅𝐎𝐑 𝐘𝐎𝐔\n\n𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝟖...',
-          "down": '✅Downloaded Successfully',
-          "error": '❌Error'
-        }
-    },
-
-start: async function({ nayan, events, args, lang }) {
-    const axios = require("axios")
-    const request = require("request")
-    const fs = require("fs-extra")
-  const { ytdown, ndown, tikdown, twitterdown } = require("nayan-media-downloader")
-    const { messageID, threadID } = events;
-  if (!args[0]) return nayan.reply(lang("missing"), threadID, messageID);
-
-
-    let np = args.join(" ");
-   if (!args[1]) nayan.reply(lang("wait"), events.threadID, (err, info) => setTimeout(() => { nayan.unsendMessage(info.messageID) }, 20000));
-
- try {
-    const res = await ndown(`${np}`);
-console.log(res)
-    var msg = [];
-    let img1 = `${res.data[0].url}`;
-
-
-    let imgs1 = (await axios.get(`${img1}`, {
-        responseType: 'arraybuffer'
-    })).data;
-    fs.writeFileSync(__dirname + "/cache/fbvideo.mp4", Buffer.from(imgs1, "utf-8"));
-    var allimage = [];
-    allimage.push(fs.createReadStream(__dirname + "/cache/fbvideo.mp4"));
-
-    {
-        msg += lang("down")
-    }
-
-    return nayan.reply({
-        body: msg,
-        attachment: allimage
-    }, events.threadID, events.messageID);
-} catch (err) {
-    nayan.reply(lang("error"), events.threadID, events.messageID);  
-   }
-}
+module.exports.config = {
+  name: "fbmp4",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "HungCho",
+  description: "",
+  commandCategory: "Media",
+  usages: "/fbmp4 [link video] dạng 1000xxxx/video/idviddeo",
+  cooldowns: 10,
+  dependencies: {
+    "request": "",
+    "fs": "",
+    "node-cmd": "",
+    "ytdl-core": "",
+    "fb-tools": ""
+  }
 };
+
+module.exports.run = async({api,event,args}) => {
+  const fs = require("fs");
+ const facebookTools = global.nodemodule["fb-tools"];
+ const axios = require("axios");
+ const name = args.join(" ")
+  videoObj = await facebookTools.getVideoUrl(name);
+  var callback = () => api.sendMessage({body: `Hi ?`,attachment: fs.createReadStream(__dirname + "/src/a2.mp3")}, event.threadID, () => fs.unlinkSync(__dirname + "/src/a2.mp3")); 
+      return request(encodeURI(`${videoObj.sdLink}`)).pipe(fs.createWriteStream(__dirname+"/src/a2.mp3")).on("close",() => callback());
+       };
