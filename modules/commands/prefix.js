eebe06d2ada9e398c763f13d3cb3f4833c3a30f4
raihanbot2 <azizulislam20022r@gmail.com>
2024-08-26 09:48:07 +0600

    Update prefix.js

--- modules/commands/prefix.js
+++ modules/commands/prefix.js
@@ -1,39 +1,59 @@
+const dipto = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+const moment = require('moment-timezone');
+const pathFile = __dirname + '/cache/d1pt0.txt';
+if (!fs.existsSync(pathFile))
+  fs.writeFileSync(pathFile, 'true');
+  const isEnable = fs.readFileSync(pathFile, 'utf-8');
 module.exports.config = {
   name: "prefix",
-  version: "1.0.1",
-  hasPermssion: 0,
-  credits: "Mr Chand",
-  description: "See the bot prefix",
-  commandCategory: "For admin",
-  usages: "out prefix",
+  version: "1.0.0",
+  permission: 2,
+  credits: "dipto",
+  prefix:true,
+  description: "when send ,prefix then response",
+  category: "bot prefix",
+  usages: "prefix",
   cooldowns: 5,
 };
-
-module.exports.handleEvent = async ({ event, api, Threads }) => {
-  var { threadID, messageID, body, senderID } = event;
-  //if (senderID == global.data.botID) return;
-  if ((this.config.credits) != "Mr Chand") { return api.sendMessage(`Changed credits!`, threadID, messageID)}
-  function out(data) {
-    api.sendMessage(data, threadID, messageID)
+module.exports.handleEvent = async ({ api, event }) => {
+  if (isEnable == "true"){
+  const dipto2 = event.body ? event.body.toLowerCase() : '';
+    const GP = "🤍✨𝐑𝐎𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗✨🤍";
+     let d1PInfo = await api.getThreadInfo(event.threadID);
+  let diptoName = d1PInfo.threadName;
+    var time = moment.tz("Asia/Dhaka").format("LLLL");
+  const text = `╭•┄┅═══❁🌺❁═══┅┄•╮\n${GP}\n╰•┄┅═══❁🌺❁═══┅┄•╯\n\n𝐁𝐎𝐓 𝐍𝐀𝐌𝐄 : ${global.config.BOTNAME}\n𝐑𝐎𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 : ｢ ${global.config.PREFIX} ｣\n𝐑𝐎𝐁𝐎𝐓 𝐂𝐌𝐃: ｢ ${client.commands.size} ｣\n𝐓𝐈𝐌𝐄 : ${time}\n𝐆𝐑𝐎𝐔𝐏 𝐍𝐀𝐌𝐄: ${diptoName}\n`
+  //const text2 = text[Math.floor(Math.random() * text.length)];
+const imgur = ["https://i.imgur.com/iiHIxsU.mp4",               "https://i.imgur.com/iiHIxsU.mp4",    "https://i.imgur.com/iiHIxsU.mp4",  "https://i.imgur.com/iiHIxsU.mp4",  "https://i.imgur.com/iiHIxsU.mp4",  "https://i.imgur.com/iiHIxsU.mp4",  "https://i.imgur.com/iiHIxsU.mp4", "https://i.imgur.com/iiHIxsU.mp4",  "https://i.imgur.com/iiHIxsU.mp4",  "https://i.imgur.com/iiHIxsU.mp4"]
+  const link = imgur[Math.floor(Math.random() * imgur.length)];
+  const res = await dipto.get(link, { responseType: 'arraybuffer' })
+const ex = path.extname(link);
+  const filename = __dirname + `/cache/dipto3${ex}`;
+  fs.writeFileSync(filename, Buffer.from(res.data, 'binary'));
+  if (dipto2.indexOf("prefix") ===0|| dipto2.indexOf("Prefix") ===0 )
+  {
+api.sendMessage({body:`${text}`,attachment: fs.createReadStream(filename)},event.threadID,() => fs.unlinkSync(filename),event.messageID)
   }
-  var dataThread = (await Threads.getData(threadID));
-  var data = dataThread.data; 
-  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+ }
+}
+module.exports.run = async ({api,args, event}) => {
+try {
+  if (args[0] == 'on') {
+    fs.writeFileSync(pathFile, 'true');
+    api.sendMessage('no prefix on successfully', event.threadID, event.messageID);
+  }
+  else if (args[0] == 'off') {
+    fs.writeFileSync(pathFile, 'false');
+    api.sendMessage('no prefix off successfully', event.threadID, event.messageID);
+  }
+  else if (!args[0]){
+    api.sendMessage(`Wrong format ${this.config.name}use off/on`, event.threadID, event.messageID);
+  }
+  }
+  catch(e) {
+    console.log(e);
+  }
 
-  var arr = ["mpre","mprefix","prefix", "dấu lệnh", "prefix của bot là gì","daulenh", "duong"];
-  arr.forEach(i => {
-    let str = i[0].toUpperCase() + i.slice(1);
-    if (body === i.toUpperCase() | body === i | str === body) {
-const prefix = threadSetting.PREFIX || global.config.PREFIX;
-      if (data.PREFIX == null) {
-        return out(`️️️️️️️️️️️️️️️️️️️️️️️️️🚀RAIHAN-ROBOT PREFIX ⇉ [ ${prefix} ]`)
-      }
-      else return out(`️️️️️️️️️️️️️️️️️️️️️️️️️🛸RAIHAN-ROBOT PREFIX  ⇉ 👉🏻 [ ${prefix} ]  `    + data.PREFIX )
-    }
-
-  });
-};
-
-module.exports.run = async({ event, api }) => {
-    return api.sendMessage(`️️️️️️️️️️️️️️️️️️️️️️️️️This is my prefix⇉ [ ${global.config.PREFIX} ]`, event.threadID)
-      }
+}
