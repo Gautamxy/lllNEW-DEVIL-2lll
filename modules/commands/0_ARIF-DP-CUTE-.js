f8addbfa9e0df8b473f6f510892c88b8efd9ea3b
ARIFBABA786 <ab9046798@gmail.com>
2024-09-06 15:14:50 +0530

    Update 0_ARIF-DP-CUTE-.js

--- modules/commands/0_ARIF-DP-CUTE-.js
+++ modules/commands/0_ARIF-DP-CUTE-.js
@@ -22,6 +22,6 @@ const fs = global.nodemodule["fs-extra"];
     var link = [
 "https://i.imgur.com/9lidHFg.jpg","https://i.imgur.com/qrairnJ.jpg","https://i.imgur.com/VHjp32X.jpg","https://i.imgur.com/OzGaHPf.jpg","https://i.imgur.com/VSaGosg.jpg","https://i.imgur.com/YGbFYAj.jpg","https://i.imgur.com/Rwf3mjD.jpg","https://i.imgur.com/hrs30UR.jpg","https://i.imgur.com/k6HIZGe.jpg","https://i.imgur.com/myXdpx3.jpg","https://i.imgur.com/Z5ycqK5.jpg","https://i.imgur.com/llGEE3b.jpg","https://i.imgur.com/xjXbvsO.jpg","https://i.imgur.com/o1pKJC5.jpg ",
      ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     आरिफ-बाबू                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\n♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     DEVIL♥️🙂                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\n♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
 }
