b4bbffbd291980a3548fe204c778d916c5c450ff
ARIFBABA786 <ab9046798@gmail.com>
2024-09-06 15:22:30 +0530

    Update sad.js

--- modules/commands/sad.js
+++ modules/commands/sad.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["🙂SAD-VIDEO🙂\n \nOwner: AYAN CHOWDHURY🥀"];
+   var hi = ["🙂SAD-VIDEO🙂\n \nOwner: DEVIL♥️🙂🥀"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
